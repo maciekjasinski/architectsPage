5cb8a286a30d743e10151979874e42e1fe5ab13b
maciekjasinski <maciek.mj00@gmail.com>
2021-02-10 18:50:47 +0100

    feat: add navbar (mobile, desktop)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home } from './Home/Home.js';
+import { Navbar } from './Components/Navbar/Navbar.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/gallery">
+            <div>Gallery</div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
