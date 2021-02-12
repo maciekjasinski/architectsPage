440730252fd546268e714d88dacbf706b0428425
maciekjasinski <maciek.mj00@gmail.com>
2021-02-12 15:12:02 +0100

    fix: path

--- src/Components/Navbar/Drawer/Drawer.js
+++ src/Components/Navbar/Drawer/Drawer.js
@@ -8,7 +8,7 @@ export function Drawer({ close }) {
             <StyledLink to="/gallery" onClick={() => close(false)}>Gallery</StyledLink>
             <StyledLink to="/projects" onClick={() => close(false)}>Projects</StyledLink>
             <StyledLink to="/certificates" onClick={() => close(false)}>Certificates</StyledLink>
-            <StyledLink to="/contacts" onClick={() => close(false)}>Contacts</StyledLink>
+            <StyledLink to="/contact" onClick={() => close(false)}>Contacts</StyledLink>
         </Wrapper>
     )
 }
